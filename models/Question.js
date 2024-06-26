8b51874a754b7a9e46254b1817c4fbe80564897d
SamaadTurner <samaad.turner@gmail.com>
2024-06-26 15:52:24 -0700

    updated syntax for questions model.

--- models/Question.js
+++ models/Question.js
@@ -1,40 +1,39 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./index');
-const Category = require('./Category');
-
-const Question = sequelize.define(
-  'Question',
-  {
-    QuestionID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    CategoryID: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Category,
-        key: 'CategoryID',
+module.exports = (sequelize, DataTypes) => {
+  const Question = sequelize.define(
+    'Question',
+    {
+      QuestionID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
       },
-      allowNull: false,
+      CategoryID: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Category',
+          key: 'CategoryID',
+        },
+        allowNull: false,
+      },
+      QuestionText: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      AnswerText: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
     },
-    QuestionText: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    AnswerText: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: false,
-    tableName: 'Questions',
-  }
-);
+    {
+      tableName: 'Questions',
+      timestamps: false,
+    }
+  );
 
-Question.belongsTo(Category, { foreignKey: 'CategoryID' });
-Category.hasMany(Question, { foreignKey: 'CategoryID' });
+  Question.associate = function (models) {
+    Question.belongsTo(models.Category, { foreignKey: 'CategoryID' });
+  };
 
-module.exports = Question;
+  return Question;
+};
